854f34f1d3eaee600a443861fffcbf0babb24b02
Myles Bennett <myles.bennett@hotmail.com>
2019-03-12 13:36:52 -0400

    implemented functionality to login form to allow users to log in

--- photogur/views.py
+++ photogur/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from photogur.models import Picture, Comment
 from photogur.forms import LoginForm
+from django.contrib.auth import authenticate, login
 
 
 def root(request):
@@ -43,7 +44,20 @@ def create_comment(request):
 
 
 def login_view(request):
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            pw = form.cleaned_data['password']
+            user = authenticate(username=username, password=pw)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/home/')
+            else:
+                form.add_error('username', 'Login failed')
+    else:
+        form = LoginForm()
+
     context = {'form': form}
     http_response = render(request, 'login.html', context)
     return HttpResponse(http_response)
