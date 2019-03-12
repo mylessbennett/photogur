4bdc61a53fb04c343d817ec53357dff199d0c171
Myles Bennett <myles.bennett@hotmail.com>
2019-03-12 13:40:12 -0400

    added a logout url to allow users to log out

--- photogur/views.py
+++ photogur/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from photogur.models import Picture, Comment
 from photogur.forms import LoginForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 def root(request):
@@ -61,3 +61,8 @@ def login_view(request):
     context = {'form': form}
     http_response = render(request, 'login.html', context)
     return HttpResponse(http_response)
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/home/')
